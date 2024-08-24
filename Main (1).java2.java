1b51ffdecf15cee981f0d8bd109be1f26c0738d5
Vicky-9390 <gunuguntlavikramreddy@gmail.com>
2024-08-24 18:50:52 +0530

    Add files via upload

---
+++ Main (1).java2.java
@@ -0,0 +1,16 @@
+class Main
+{
+    public static void main(String[]args)
+    {
+        int num=0;
+        if(num==0)
+        {
+            System.out.println("Zero");
+        }
+        else
+        {
+            String result=num==0 ? "The number is positive":"The number is negative";
+            System.out.println(result);
+        }
+    }
+}
