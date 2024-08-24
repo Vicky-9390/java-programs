68a4e3b0a5ee63a8b290f69a95890f25721e4020
Vicky-9390 <gunuguntlavikramreddy@gmail.com>
2024-08-24 18:59:14 +0530

    Add files via upload

---
+++ Main (2).java
@@ -0,0 +1,11 @@
+public class Main
+{
+    public static void  main(String[]args)
+    {
+        int number =20;
+        if(number % 2 == 0)
+        System.out.println(number + "Even");
+        else
+        System.out.println(number + "odd");
+    }
+}
