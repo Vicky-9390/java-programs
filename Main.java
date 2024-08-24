8a0f050d718f6c731b1f78c957bf12de002bb14c
Vicky-9390 <gunuguntlavikramreddy@gmail.com>
2024-08-24 18:42:52 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main
+{
+    public static void main(String[]args)
+    {
+        int num=4;
+        
+        if(num>3)
+                System.out.println("The number is positive");
+        else if (num>1)
+                System.out.println("The number is Negative");
+        else
+                 System.out.println("Zero");
+            
+    }
+}
---
+++ Main.java1.java
@@ -0,0 +1,17 @@
+class Main
+  {
+      public static void main(String[]args){
+          int num =10;
+     
+     if(num<=5)
+     {
+         if(num==0)
+         System.out.println("Zero");
+         else
+         System.out.println("The number is positive");
+     }
+        else
+        System.out.println("The number is negative");
+          
+      }
+  }  
